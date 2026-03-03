df1137ba948ac958f6233759a8f6013e5dc7c9ff
Slobodan1989 <slobodan@tellet.ai>
2026-03-03 16:55:51 +0100

    favicon fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,9 +43,12 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: "/favicon.ico", sizes: "any" },
       { url: "/images/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/images/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
+    shortcut: "/favicon.ico",
+    apple: "/images/apple-touch-icon.png",
   },
   openGraph: {
     title: "Delovi za prskalice i atomizere | Kosmos Promet",
